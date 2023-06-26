95898e054312afdb663c5daa52350c1273b7e642
DanielAlejandroRivero <riverodaniel.89@gmail.com>
2023-06-25 22:53:08 -0300

    Cambios realizados en archivo JS

--- js/app.js
+++ js/app.js
@@ -1,42 +1,43 @@
-let nombre = document.getElementById ("nombre");
-let apellido = document.getElementById ("apellido");
-let correo = document.getElementById ("correo");
-let quantitybtn = document.getElementById ("ticketsQuantity");
-let categorybtn = document.getElementById ("category");
-let pricebtn = document.getElementById ("resumen");
-let deletebtn = document.getElementById ("clear");
-let resultado = document.getElementById ("price")
-let estudiante = document.getElementById ("estudiante");
-let trainee = document.getElementById ("trainee");
-let junior = document.getElementById ("junior");
-
-
-
-
-function ticketsPrice(evento) {
-    evento.preventDefault();
-    
-    let quantitybtn = document.querySelector();
-
-    if (Number(quantitybtn.value)) {
-        let totalPayment;
-
-        switch (categorybtn.value) {
-            case "estudiante": {
-                totalPayment = 200 * quantitybtn.value * 0.2;
-                break;
-            }
-            case "trainee": {
-                totalPayment = 200 * quantitybtn.value * 0.5;
-                break;
-            }
-            case "junior": {
-                totalPayment = 200 * quantitybtn.value * 0.85;
-                break;
-            }
-        }
-
-        document.querySelector(resultado.value).textContent = {totalPayment};
+function calcularPrecioTotal() {
+    // Obtener los valores ingresados por el cliente
+    var cantidad = parseInt(document.getElementById('ticketsQuantity').value);
+    var categoria = document.getElementById('category').value;
+    var nombre = document.getElementById('nombre').value;
+    var apellido = document.getElementById('apellido').value;
+    var correo = document.getElementById('mail').value;
+  
+    // Calcular el precio total según la cantidad
+    var precioUnitario = 200; // Valor del ticket
+    var precioTotal = cantidad * precioUnitario;
+  
+    // Aplicar descuento según la categoría
+    if (categoria === 'Estudiante') {
+      precioTotal *= 0.2; // Aplicar descuento del 80%
+    } else if (categoria === 'Trainee') {
+      precioTotal *= 0.5; // Aplicar descuento del 50%
+    } else if (categoria === 'Junior') {
+      precioTotal *= 0.85; // Aplicar descuento del 15%
     }
 
+    if (nombre === '' || apellido === '' || correo === '' || cantidad === '' || categoria === '') {
+        alert('Por favor, complete todos los campos requeridos.');
+        return false; 
+    }
+  
+    // Mostrar el precio total
+    document.getElementById('price').textContent = 'Total a pagar: $' + precioTotal.toFixed();
+    return false; 
+}
+  
+
+function borrarCampos() {
+    document.getElementById('nombre').value = '';
+    document.getElementById('apellido').value = '';
+    document.getElementById('mail').value = '';
+    document.getElementById('ticketsQuantity').value = '';
+    document.getElementById('category').selectedIndex = 0;
+
+    document.getElementById('price').textContent = 'Total a pagar: $';
+
+    return false; // Evitar el envío del formulario
 }
